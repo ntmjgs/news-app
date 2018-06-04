f61ed84bf0515321421491694d472e952046067b
Zhu <fezhu@ebay.com>
2018-06-04 22:03:38 +0800

    新闻小程序

---
+++ pages/detail/detail.js
@@ -0,0 +1,49 @@
+Page({
+  data:{
+    'newsId':'',
+    'newsDetail':{}
+  },
+  onLoad(option){
+    let id = option.id
+    this.setData({
+      newsId:id
+    })
+    this.getNewsDetail()
+  },
+  goback(){
+    wx.navigateBack()
+  },
+  //自定义导航栏看不到下拉刷新的...,因此用loading页面显示刷新效果
+  onPullDownRefresh() {
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    this.getNewsDetail(() => {
+      setTimeout(() => {
+        wx.stopPullDownRefresh()
+        wx.hideLoading()
+      }, 500)
+    });
+
+  },
+  getNewsDetail(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {       
+        id: this.data.newsId
+      },
+      success: res => {
+        let result = res.data.result;
+        result.date = result.date.split('T')[1].substr(0, 5);
+        this.setData({
+          newsDetail:result
+        })
+        
+      },
+      complete: () => {
+        callback && callback()
+
+      }
+    })
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,71 @@
 Page({
   data:{
     firstnews:{},
-    newslist: []
+    news: [],
+    navid:'gn' ,
+    navItems :[
+      { 'id': 'gn', 'text': '国内' },
+      { 'id': 'gj', 'text': '国际' },
+      { 'id': 'cj', 'text': '财经' },
+      { 'id': 'yl', 'text': '娱乐' },
+      { 'id': 'js', 'text': '军事' },
+      { 'id': 'ty', 'text': '体育' },
+      { 'id': 'other', 'text': '其他' }
+    ],
+    defaultImage:'/images/default.jpg'
   },
-  onLoad(){
-    let formerfirst = {
-      "id": 1519631218506,
-      "title": "外媒称香港回归15年打破“经济将死”预言",
-      "date": "09:34",
-      "source": "中国新闻网",
-      "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-
-    };
-    let formerlist = [{
-      "id": 1519631218591,
-      "title": "实德指媒体窃公司机密 已向某媒体递律师函",
-      "date": "2012-04-21T11:32:04.000Z",
-      "source": "腾讯财经",
-      "firstImage": "http://img1.gtimg.com/finance/pics/hv1/33/207/1023/66573393.jpg"
-    },
-    {
-      "id": 1519631218595,
-      "title": "公务员医疗费用成迷 学者呼吁管理应公开透明",
-      "date": "2012-02-26T08:13:00.000Z",
-      "source": "财新网",
-      "firstImage": "http://img1.gtimg.com/finance/pics/hv1/241/102/983/63945826.jpg"
-    }]
-    let afterlist = formerlist.map((list)=>{
-      list.date = list.date.split('T')[1].substr(0,5);
-      return list
+  onLoad() {
+    this.requestData()
+  },
+  onTapNews(event){
+    let newsId = event.currentTarget.dataset.index
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + newsId,
     })
+  },
+  //自定义导航栏看不到下拉刷新的... 因此用loading框显示刷新效果
+  onPullDownRefresh() {
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    this.requestData(() => {
+      setTimeout(()=>{
+        wx.stopPullDownRefresh()
+        wx.hideLoading()
+      },500)
+    });
+  },
+  tapNav(event){
+    let navid = event.target.dataset.navid;
     this.setData({
-      firstnews:formerfirst,
-      newslist:afterlist
+      navid: navid
     })
-
+    this.requestData()
+  },
+  requestData(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.navid
+      },
+      success: res => {
+        let result = res.data.result;
+        if(!!result){
+          result = result.map((list) => {
+            list.date = list.date.split('T')[1].substr(0, 5);
+            return list
+          })
+          let firstnews = result[0];
+          let newslist = result.slice(1);
+          this.setData({
+            firstnews: firstnews,
+            news: newslist
+          })
+        }
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   }
 })
